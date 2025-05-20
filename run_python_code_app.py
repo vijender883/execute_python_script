33227249be09f23f678ad328791cec3c932c1285
VamshiVardhanKarnati <vamshivardhankarnati4352@gmail.com>
2025-05-20 15:06:44 +0530

    added /health route

--- run_python_code_app.py
+++ run_python_code_app.py
@@ -222,5 +222,10 @@ lines = globals()["__builtins__"]["print"].__self__._getframe().f_back.f_locals[
         if os.path.exists(temp_filename):
             os.remove(temp_filename)
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "healthy"})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=False)
